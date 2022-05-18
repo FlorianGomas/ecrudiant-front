8f5fa30ab8c5db34c5de95c2b7b1188581fea96c
fgomas <florian.gomas@etu.u-bordeaux.fr>
2022-05-18 11:23:50 +0200

    Ajout modele studentInfo

---
+++ src/app/studentInfo.ts
@@ -0,0 +1,10 @@
+export interface StudentInfo{
+    id : number;
+    lastName : string;
+    firstName : string;
+    birthday : string;
+    company : string;
+    jobTitle : string;
+    email : string;
+    picture : string;
+}
