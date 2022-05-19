9889aa852f401eb5d3c1785cdeac63a9dfdf2414
fgomas <florian.gomas@etu.u-bordeaux.fr>
2022-05-19 10:40:07 +0200

    Ajout des validators pour le formulaire de modification d'un étudiant

--- src/app/edit-form/edit-form.component.ts
+++ src/app/edit-form/edit-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StudentInfo } from '../studentInfo';
 import { StudentService } from '../student.service';
@@ -13,15 +13,17 @@ export class EditFormComponent implements OnInit {
 
   student = new StudentInfo();
   studentForm:FormGroup;
+  dateRegex = '^([0]?[1-9]|[1|2][0-9]|[3][0|1])[/]([0]?[1-9]|[1][0-2])[/]([0-9]{4}|[0-9]{2})$'
+  emailRegex = '[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}'
 
   constructor(private route:ActivatedRoute, private studentService:StudentService, public fb: FormBuilder, public router : Router) {
     this.studentForm = fb.group({
-      lastName: new FormControl(),
-      firstName: new FormControl(),
-      birthday: new FormControl(),
+      lastName: new FormControl(null, [Validators.required]),
+      firstName: new FormControl(null, [Validators.required]),
+      birthday: new FormControl(null, [Validators.required, Validators.pattern(this.dateRegex)]),
       company: new FormControl(),
       jobTitle: new FormControl(),
-      email: new FormControl()
+      email: new FormControl(null, Validators.pattern(this.emailRegex))
     });  
   }
 
@@ -65,7 +67,8 @@ export class EditFormComponent implements OnInit {
   }
 
   submit(){
-    const studentId = parseInt(this.route.snapshot.paramMap.get('id')!);
+    if (this.studentForm.valid){
+      const studentId = parseInt(this.route.snapshot.paramMap.get('id')!);
     this.student.firstName = this.studentForm.get('firstName')?.value;
     this.student.lastName = this.studentForm.get('lastName')?.value;
     this.student.birthday = this.studentForm.get('birthday')?.value;
@@ -80,6 +83,7 @@ export class EditFormComponent implements OnInit {
           console.log("PUT call in error", response);
       });
       this.router.navigate(['/etudiant/'+ this.student.id]);
+    }  
   }
 
 }
